a2f8956f208dd82fcaf01ef5494b8925ab19524c
caw <dominik.wozniewicz@gmail.com>
2023-02-25 07:01:04 +0100

    back to tsx

--- src/containers/Showcase/Showcase.tsx
+++ src/containers/Showcase/Showcase.tsx
@@ -2,7 +2,7 @@ import Project from '../../components/Project/Project';
 import './Showcase.css';
 import AnimText from '../../components/TypeAnimation/AnimText';
 
-const Showcase = () => {
+const Showcase: React.FunctionComponent = () => {
   return (
     <div className="showcase">
       <AnimText animText="Moje projekty" animWrapper="h1" animSpeed={40} />
