1fd6793b69a25bfe43216bc614492cbfff0b574d
caw <dominik.wozniewicz@gmail.com>
2023-03-06 14:40:51 +0100

    Project showcase images

--- src/containers/Showcase/Showcase.tsx
+++ src/containers/Showcase/Showcase.tsx
@@ -3,17 +3,24 @@ import './Showcase.css';
 import AnimText from '../../components/TypeAnimation/AnimText';
 import { pomoImages } from '../../images/images';
 import { cartImages } from '../../images/images';
+import { budgetImages } from './../../images/images';
 
 const Showcase: React.FunctionComponent = () => {
-  // TODO IMAGE RESIZING
   const [pomo1, pomo2, pomo3] = pomoImages;
   const [cart1, cart2, cart3] = cartImages;
+  const [budget1, budget2, budget3] = budgetImages;
 
   return (
     <div className="showcase">
       <AnimText animText="Moje projekty" animWrapper="h1" />
       <Project title="Pomodoro" img1={pomo1} img2={pomo2} img3={pomo3} />
       <Project title="Shopping Cart" img1={cart1} img2={cart2} img3={cart3} />
+      <Project
+        title="Budget Planner"
+        img1={budget1}
+        img2={budget2}
+        img3={budget3}
+      />
     </div>
   );
 };
--- src/images/images.tsx
+++ src/images/images.tsx
@@ -6,6 +6,12 @@ import cart1 from '../assets/project_02/cart_01.png';
 import cart2 from '../assets/project_02/cart_02.png';
 import cart3 from '../assets/project_02/cart_03.png';
 
+import budget1 from '../assets/project_03/budget_01.png';
+import budget2 from '../assets/project_03/budget_02.png';
+import budget3 from '../assets/project_03/budget_03.png';
+
 export const pomoImages = [pomo1, pomo2, pomo3];
 
 export const cartImages = [cart1, cart2, cart3];
+
+export const budgetImages = [budget1, budget2, budget3];
